32e7312e89b49379774981228efd56059c604d39
Paul Tee <jianhaoti@gmail.com>
2024-01-29 04:04:19 -0500

    some resizing

--- src/AlgoCard.tsx
+++ src/AlgoCard.tsx
@@ -1,44 +1,36 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-interface OutlinedCardProps {
+interface AlgoCardProps {
     title: string;
-    subtitle: string;
+    grouping: string;
     description: string;
     backgroundColor: string;
 }
 
 const colors = ['#A0ACBB', '#BDB0BF', '#FADBE4']
-const OutlinedCard: React.FC<OutlinedCardProps> = ({ title, subtitle, description, backgroundColor }) => {
+const AlgoCard: React.FC<AlgoCardProps> = ({ title, grouping, description, backgroundColor }) => {
 
     return (
         <Box sx={{ minWidth: 275 }}>
         <Card variant="outlined" sx={{backgroundColor}}>
             <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                {subtitle}
-            </Typography>
-            <Typography variant="h5" component="div">
-                {title}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-            </Typography>
-            <Typography variant="body2">
-                {description}
-            </Typography>
+                <Typography variant="h5" component="div">
+                    {title}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    {grouping}
+                </Typography>
+                <Typography variant="body2">
+                    {description}
+                </Typography>
             </CardContent>
-            <CardActions>
-            <Button size="small">Learn More</Button>
-            </CardActions>
         </Card>
         </Box>
     );
 };
 
-export default OutlinedCard;
+export default AlgoCard;
--- src/AlgoRoom.tsx
+++ src/AlgoRoom.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Box, ImageList, ImageListItem } from '@mui/material';
 import AlgoCard from './AlgoCard'; // Import your modified OutlinedCard component
 
 const AlgoRoom: React.FC = () => {
-    const colors = ['#8898AA', '#C7BCC8', '#F4E4EA']
+    const colors = ['#8898AA', '#C7BCC8', '#F4E4EA'];
+
+    const [isFullTitle, setIsFullTitle] = useState(true);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsFullTitle(window.innerWidth > 1200);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
 
     const cardData = [
-        { title: 'Card 1', subtitle: 'Subtitle 1', description: 'Description 1' },
-        { title: 'Card 2', subtitle: 'Subtitle 2', description: 'Description 2' },
-        { title: 'Card 1', subtitle: 'Subtitle 1', description: 'Description 1' },
-        { title: 'Card 2', subtitle: 'Subtitle 2', description: 'Description 2' },
-        { title: 'Card 1', subtitle: 'Subtitle 1', description: 'Description 1' },
-        { title: 'Card 2', subtitle: 'Subtitle 2', description: 'Description 2' },
-        { title: 'Card 1', subtitle: 'Subtitle 1', description: 'Description 1' },
-        { title: 'Card 2', subtitle: 'Subtitle 2', description: 'Description 2' },
-        { title: 'Card 1', subtitle: 'Subtitle 1', description: 'Description 1' },
-    ];
+        { title: 'BFS', grouping: 'Search', description: 'A Drop of Water' },
+        { title: 'DFS', grouping: 'Search', description: 'Drill the Depths' },
+        { title: 'Dijkstra', grouping: 'Shortest Path', description: 'Walk Radially' },
+        { title: 'Prim', grouping: 'MST', description: 'Connect the Dots' },
+        { title: 'Kruskal', grouping: 'MST', description: 'Thinnest Threads' },
+        { title: 'Bellman-Ford', grouping: 'Shortest Path', description: 'Negative Dijkstra' },
+        { title: 'A*', grouping: 'Search', description: 'A Guided Wanderer' },
+        { title: 'Tarjan', grouping: 'Search', description: 'Unraveling the Knots' },
+        { title: 'Ford-Fulkerson', grouping: 'Flows & Cuts', description: 'Channel the Currents' },
+        { title: 'Gale-Shapley', grouping: 'Matchings', description: 'Stable Marriage' },
+        { title: 'Karger', grouping: 'Cuts', description: 'Connected Minima' },
 
+    ];
+    
     return (
-        <Box sx={{  
+        <Box sx={{
             padding: '20px', 
             maxHeight: '60vh',
             overflow: 'auto', 
@@ -33,17 +50,17 @@ const AlgoRoom: React.FC = () => {
                 backgroundColor: '#E3C46E',
             },
         }}>
-            <ImageList cols={3} gap={8}>
+            <ImageList cols={3} gap={6}>
                 {cardData.map((item, index) => {
-                    const row = Math.floor(index / colors.length);
+                    const row = Math.floor(index / 3);
                     const colorIndex = (index + row) % colors.length;
                     const color = colors[colorIndex];
 
                     return (
                         <ImageListItem key={index}>
                             <AlgoCard
-                                title={item.title}
-                                subtitle={item.subtitle}
+                                title={isFullTitle ? item.title : item.title.split(' ').map(word => word[0]).join('')}
+                                grouping={item.grouping}
                                 description={item.description}
                                 backgroundColor={color}
                             />
@@ -53,7 +70,7 @@ const AlgoRoom: React.FC = () => {
             </ImageList>
         </Box>
     );
-
 };
 
 export default AlgoRoom;
+
