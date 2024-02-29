9ef84678cc494897b063affff4e7ef32c0043b24
Paul Tee <jianhaoti@gmail.com>
2024-02-28 19:44:51 -0500

    bfs logic done

--- src/bfs.ts
+++ src/bfs.ts
@@ -1,6 +1,7 @@
 export type StepType = {
   visited: string[];
   queue: string[];
+  processing: string;
 };
 
 export const bfs = (
@@ -11,37 +12,46 @@ export const bfs = (
   let steps: StepType[] = [];
   let visited: Set<string> = new Set();
   let queue: string[] = [source];
-
-  // this temp is to give a sense of "steps = layers of bfs"
-  let temp: string[] = [];
+  let processing: string = source;
 
   while (queue.length > 0) {
-    // get the current layers' neighbors, then push them into visited
-    while (queue.length > 0) {
-      const node = queue.shift()!; // Assume non-null assertion is safe here
-      if (!visited.has(node)) {
-        visited.add(node); // Mark the node as visited upon dequeuing
+    let currentLayer: string[] = [];
+    let newQueue: string[] = [];
 
-        const neighbors = graph.get(node) || [];
-        for (const neighbor of neighbors) {
-          if (!visited.has(neighbor)) {
-            temp.push(neighbor);
-          }
+    while (queue.length > 0) {
+      const node = queue.shift()!;
+      processing = node;
+
+      const neighbors = graph.get(node) || [];
+      for (const neighbor of neighbors) {
+        if (!visited.has(neighbor)) {
+          newQueue.push(neighbor);
+          currentLayer.push(neighbor);
         }
       }
+      // added in all the neighbors
+      steps.push({
+        visited: Array.from(visited),
+        queue: queue.concat(newQueue), // Reflects the current state of the queue
+        processing: processing,
+      });
+
+      visited.add(node);
+      processing = "";
+
+      // added in new visited
+      steps.push({
+        visited: Array.from(visited),
+        queue: queue.concat(newQueue), // Reflects the current state of the queue
+        processing: processing,
+      });
     }
-    // repopulate, if possible, the empty queue with the next layer from temp
-    const currentLayer: string[] = [];
-    while (temp.length > 0) {
-      const qItem = temp.pop();
-      if (qItem && !currentLayer.includes(qItem)) {
-        queue.push(qItem);
-        currentLayer.push(qItem);
-      }
-    }
+
     if (currentLayer.length > 0) {
       layers.push(currentLayer);
     }
+
+    queue = newQueue; // Prepare for the next layer
   }
 
   return { steps, layers };
