9efb904055cccadb5bab1ae6a508b30e4e4e0537
Paul Tee <jianhaoti@gmail.com>
2024-03-08 17:45:12 -0500

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import "./App.css";
 import Graph from "./Graph";
-import ControlRoom from "./controlRoom";
+import ControlRoom from "./ControlRoom";
 import { Button, ButtonGroup } from "@mui/material";
 import { ReactComponent as AlgoIcon } from "./assets/algoIcon.svg";
 import { ReactComponent as DataIcon } from "./assets/dataIcon.svg";
